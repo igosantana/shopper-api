092990391012f35846ecbab4bade86840548ef18
Igo Santana <igo.santana33@gmail.com>
2023-03-17 17:30:49 -0300

    feat: add cors

--- src/modules/users/useCases/getUsersCase/GetUsersController.ts
+++ src/modules/users/useCases/getUsersCase/GetUsersController.ts
@@ -6,6 +6,6 @@ export class GetUsersController {
     const getUserCase = new GetUsersUseCase()
     const result = await getUserCase.execute()
     if (result == "users_not_found") res.status(400).json({ message: "fail" })
-    res.status(200).json({ data: result })
+    res.status(200).json(result)
   }
 }
